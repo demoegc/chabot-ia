9f01551c5e1da5b9785cddb661b872ee8074ce20
jpegconnects <jesuan.patermin@egconnects.com>
2025-07-16 17:20:23 -0400

    cambiando la condición del if

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/webhook', async (req, res) => {
     let message = messages[0]
     const { chatId, type, sentFromApp, authorName, authorId, status } = message;
 
-    if (chatId !== '19545480212' && chatId !== '584129253568') {
+    if (chatId !== '19545480212') {
       console.log('chatId', chatId, 'Es diferente a 19545480212')
       return res.end()
     }
